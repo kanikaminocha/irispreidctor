5a1b398fdc4338e95a0d6002f4c8ea8d72ab04fd
kanikaminocha <106899018+kanikaminocha@users.noreply.github.com>
2022-06-05 12:59:15 +0800

    Add files via upload

---
+++ app.py
@@ -0,0 +1,35 @@
+import numpy as np
+from flask import Flask, request, jsonify, render_template
+import pickle
+import pandas as pd
+
+app = Flask(__name__)
+model = pickle.load(open('irisPredictor.mdl', 'rb'))
+
+
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/predict',methods=['POST'])
+def predict():
+    '''
+    For rendering results on HTML GUI
+    '''
+   
+    sepalLength= float(request.form['sepalLength'])
+    sepalWidth = float(request.form['sepalWidth'])
+    petalLength  = float(request.form['petalLength'])
+    petalWidth = float(request.form['petalWidth'])
+    finalFeatures = np.array([[sepalLength,sepalWidth,petalLength,petalWidth]])
+    prediction = model.predict(finalFeatures)
+
+
+    
+
+    return render_template('index.html', prediction_text='It has been identified as  $ {}'.format(prediction[0]))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
